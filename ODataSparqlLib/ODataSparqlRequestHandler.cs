61b3d87236df8583342acb1ccddf081f7a56522b
Kal Ahmed <kal@brightstardb.com>
2013-01-26 15:04:22 +0000

    Removed unusued static fields and fixed warning about possible null value for AcceptTypes

--- ODataSparqlLib/ODataSparqlRequestHandler.cs
+++ ODataSparqlLib/ODataSparqlRequestHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Web;
 using Microsoft.Data.OData;
 using Microsoft.Data.OData.Query;
@@ -8,9 +7,6 @@ namespace ODataSparqlLib
 {
     internal class ODataSparqlRequestHandler
     {
-        private static readonly string[] AtomMimeTypes = new string[] {"application/atom+xml", "application/xml"};
-        private static readonly string[] JsonMimeTypes = new string[] {"application/json"};
-        private static readonly string[] AllMimeTypes = AtomMimeTypes.Union(JsonMimeTypes).ToArray();
         private readonly ODataSparqlServiceConfiguration _config;
 
         public ODataSparqlRequestHandler(ODataSparqlServiceConfiguration serviceConfiguration)
@@ -30,9 +26,11 @@ namespace ODataSparqlLib
                 {
                     BaseUri = serviceBaseUri
                 };
-            //var formatKind = SelectOutputFormat(context.Request);
-            //messageWriterSettings.SetContentType(formatKind);
-            messageWriterSettings.SetContentType(String.Join(",", context.Request.AcceptTypes), context.Request.Headers["Accept-Charset"]);
+            if (context.Request.AcceptTypes != null)
+            {
+                messageWriterSettings.SetContentType(String.Join(",", context.Request.AcceptTypes),
+                                                     context.Request.Headers["Accept-Charset"]);
+            }
             var responseMessage = new HttpDataResponseMessage(context.Response);
             var feedGenerator = new ODataFeedGenerator(
                 responseMessage, 
@@ -43,17 +41,5 @@ namespace ODataSparqlLib
             context.Response.ContentType = "application/atom+xml";
         }
 
-        ODataFormat SelectOutputFormat(HttpRequest request)
-        {
-            var header = request.Headers["AcceptType"];
-            var bestMatch = MimeParse.BestMatch(AllMimeTypes, request.Headers["Accept"]);
-            if (string.IsNullOrEmpty(bestMatch))
-            {
-                throw new Exception("No valid mime-type in request header");
-            }
-            if (AtomMimeTypes.Contains(bestMatch)) return ODataFormat.Atom;
-            if (JsonMimeTypes.Contains(bestMatch)) return ODataFormat.VerboseJson;
-            throw new Exception("Could not match mime-types in request header");
-        }
     }
 }
